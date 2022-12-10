771b18d5314f1f68f39a3d412226d7c50b0af60a
Pablo Hernandez <hpablo575@gmail.com>
2022-12-09 20:51:04 -0700

    dedeclared the two variables we will use

--- stringMonth.java
+++ stringMonth.java
@@ -1,7 +1,15 @@
+//import JOptionPane 
 import javax.swing.JOptionPane;
+
+
 public class stringMonth {
 
     public static void main(String[] args) {
+        // I am a comment inside of the main method
+
+        // declaring string variables month and season
+        String month;
+        String season;
 
     }
 }
